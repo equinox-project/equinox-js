1f4814ed6ec6233e69925440839f51071fbf08e6
Einar Norðfjörð <einar@nordfjord.io>
2023-03-13 21:37:49 -0400

    docs edit link

--- apps/docs/docusaurus.config.js
+++ apps/docs/docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl: "https://github.com/nordfjord/equinox-js",
+          editUrl: "https://github.com/nordfjord/equinox-js/tree/main/apps/docs",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
