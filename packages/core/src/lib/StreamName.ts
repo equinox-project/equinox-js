678eb683f64d8d9180961c600b31dce401a137e4
Einar Norðfjörð <gloogankle@gmail.com>
2023-07-25 13:03:27 -0400

    fix: streamname id separator

--- packages/core/src/lib/StreamName.ts
+++ packages/core/src/lib/StreamName.ts
@@ -1,22 +1,23 @@
 export const compose = (category: string, id: string) => `${category}-${id}`;
+const CATEGORY_SEPARATOR = '-';
+const ID_SEPARATOR = '_';
 
 export function parseCategoryAndId(streamName: string) {
-  const idx = streamName.indexOf('-');
+  const idx = streamName.indexOf(CATEGORY_SEPARATOR);
   return [streamName.slice(0, idx), streamName.slice(idx + 1)];
 }
 
 export function parseCategory(streamName: string) {
-  const idx = streamName.indexOf('-');
+  const idx = streamName.indexOf(CATEGORY_SEPARATOR);
   return streamName.slice(0, idx);
 }
 
 export function parseIds(streamName: string) {
-  const idx = streamName.indexOf('-');
-  return streamName.slice(idx + 1).split('-');
+  return parseId(streamName).split(ID_SEPARATOR);
 }
 
 export function parseId(streamName: string) {
-  const idx = streamName.indexOf('-');
+  const idx = streamName.indexOf(CATEGORY_SEPARATOR);
   return streamName.slice(idx + 1);
 }
 
