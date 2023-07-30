e4771c2b5c3465adc47ab9b2c53045aa0ace2cd0
Einar Norðfjörð <gloogankle@gmail.com>
2023-07-30 11:31:23 -0400

    unprettier the collapse file

--- packages/projection-pg/src/collapse.ts
+++ packages/projection-pg/src/collapse.ts
@@ -1,5 +1,7 @@
 import { Action, Change, Insert, Update, Upsert } from "./types"
 
+// Note: do not run prettier over this file, it does a bad job and makes it harder to read
+
 export function collapseChanges(changes: Change[]): Change | undefined {
   if (changes.length <= 1) return changes[0]
 
@@ -9,59 +11,31 @@ export function collapseChanges(changes: Change[]): Change | undefined {
   switch (first.type) {
     case Action.Insert:
       switch (second.type) {
-        case Action.Insert:
-          throw new Error("Cannot insert the same record twice, use Upsert")
-        case Action.Update:
-          return collapseChanges([
-            Insert({ ...first.data, ...second.data }),
-            ...changes.slice(2),
-          ])
-        case Action.Upsert:
-          return collapseChanges([Insert({ ...first.data, ...second.data }), ...changes.slice(2)])
-        case Action.Delete:
-          return collapseChanges(changes.slice(2))
+        case Action.Insert: throw new Error("Cannot insert the same record twice, use Upsert")
+        case Action.Update: return collapseChanges([Insert({ ...first.data, ...second.data }), ...changes.slice(2)])
+        case Action.Upsert: return collapseChanges([Insert({ ...first.data, ...second.data }), ...changes.slice(2)])
+        case Action.Delete: return collapseChanges(changes.slice(2))
       }
     case Action.Update:
       switch (second.type) {
-        case Action.Insert:
-          throw new Error("Cannot insert after updating the same record, use Upsert")
-        case Action.Update:
-          return collapseChanges([
-            Update({ ...first.data, ...second.data }),
-            ...changes.slice(2),
-          ])
-        case Action.Upsert:
-          return collapseChanges([second, ...changes.slice(2)])
-        case Action.Delete:
-          return collapseChanges(changes.slice(1))
+        case Action.Insert: throw new Error("Cannot insert after updating the same record, use Upsert")
+        case Action.Update: return collapseChanges([Update({ ...first.data, ...second.data }), ...changes.slice(2)])
+        case Action.Upsert: return collapseChanges([second, ...changes.slice(2)])
+        case Action.Delete: return collapseChanges(changes.slice(1))
       }
     case Action.Upsert:
       switch (second.type) {
-        case Action.Insert:
-          throw new Error("Cannot insert after upserting the same record, use Upsert")
-        case Action.Update:
-          return collapseChanges([
-            Upsert({ ...first.data, ...second.data }),
-            ...changes.slice(2),
-          ])
-        case Action.Upsert:
-          return collapseChanges([
-            Upsert({ ...first.data, ...second.data }),
-            ...changes.slice(2),
-          ])
-        case Action.Delete:
-          return collapseChanges(changes.slice(1))
+        case Action.Insert: throw new Error("Cannot insert after upserting the same record, use Upsert")
+        case Action.Update: return collapseChanges([Upsert({ ...first.data, ...second.data }), ...changes.slice(2)])
+        case Action.Upsert: return collapseChanges([Upsert({ ...first.data, ...second.data }), ...changes.slice(2)])
+        case Action.Delete: return collapseChanges(changes.slice(1))
       }
     case Action.Delete:
       switch (second.type) {
-        case Action.Insert:
-          return collapseChanges([Update(second.data), ...changes.slice(2)])
-        case Action.Update:
-          throw new Error("Cannot update after deleting the same record, use Upsert")
-        case Action.Upsert:
-          return collapseChanges(changes.slice(1))
-        case Action.Delete:
-          return collapseChanges(changes.slice(1))
+        case Action.Insert: return collapseChanges([Update(second.data), ...changes.slice(2)])
+        case Action.Update: throw new Error("Cannot update after deleting the same record, use Upsert")
+        case Action.Upsert: return collapseChanges(changes.slice(1))
+        case Action.Delete: return collapseChanges(changes.slice(1))
       }
   }
 }
