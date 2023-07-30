ec2f19b50c30d68486c42c7b29e5eca2bd4a128f
Einar Norðfjörð <gloogankle@gmail.com>
2023-07-30 12:03:27 -0400

    correct

--- packages/projection-pg/src/collapse.ts
+++ packages/projection-pg/src/collapse.ts
@@ -2,14 +2,14 @@ import { Action, Change, Insert, Update, Upsert } from "./types"
 
 // Note: do not run prettier over this file, it does a bad job and makes it harder to read
 
-function concat(a: Change, b: Change): Change {
+function concat(a: Change, b: Change): Change | undefined {
   switch (a.type) {
     case Action.Insert:
       switch (b.type) {
         case Action.Insert: throw new Error("Cannot insert the same record twice, use Upsert")
         case Action.Update:
         case Action.Upsert: return Insert({ ...a.data, ...b.data })
-        case Action.Delete: return b
+        case Action.Delete: return undefined
       }
     case Action.Update:
       switch (b.type) {
@@ -41,7 +41,10 @@ export function collapseChanges(changes: Change[]): Change | undefined {
   let result = changes[0]
 
   for (let i = 1; i < changes.length; i++) {
-    result = concat(result, changes[i])
+    const res = concat(result, changes[i])
+    // an undefined means we've zeroed out the change so we skip
+    if (res == null) { result = changes[i + 1]; i++; continue; }
+    result = res
   }
   return result
 }
