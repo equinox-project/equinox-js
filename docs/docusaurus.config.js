1f0c3c62728da5be57a1e00f8b37e6d18cb0f87a
Einar Norðfjörð <gloogankle@gmail.com>
2023-08-03 09:51:51 -0400

    social card

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -54,7 +54,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: "img/docusaurus-social-card.jpg",
+      image: "img/hero_banner.png",
       navbar: {
         title: "EquinoxJS",
         logo: {
