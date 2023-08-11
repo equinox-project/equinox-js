567fd1819d3e0b0d4a79080cb051dc5bd3327ceb
Einar Norðfjörð <gloogankle@gmail.com>
2023-08-11 10:21:26 -0400

    simplify quoter as we only deal in strings

--- packages/projection-pg/src/quote.ts
+++ packages/projection-pg/src/quote.ts
@@ -1,82 +1,16 @@
-/*
- * The MIT License (MIT)
- * 
- * Copyright (c) 2014 Datalanche, Inc.
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- * 
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
-
-
 function formatDate(date) {
   date = date.replace("T", " ")
   date = date.replace("Z", "+00")
   return date
 }
 
-export function ident(value: unknown) {
-  if (value === undefined || value === null) {
-    throw new Error("SQL identifier cannot be null or undefined")
-  } else if (value === false) {
-    return '"f"'
-  } else if (value === true) {
-    return '"t"'
-  } else if (value instanceof Date) {
-    return '"' + formatDate(value.toISOString()) + '"'
-  } else if (value instanceof Buffer) {
-    throw new Error("SQL identifier cannot be a buffer")
-  } else if (Array.isArray(value)) {
-    var temp = []
-    for (var i = 0; i < value.length; i++) {
-      if (Array.isArray(value[i]) === true) {
-        throw new Error(
-          "Nested array to grouped list conversion is not supported for SQL identifier",
-        )
-      } else {
-        temp.push(quoteIdent(value[i]))
-      }
-    }
-    return temp.toString()
-  } else if (value === Object(value)) {
-    throw new Error("SQL identifier cannot be an object")
-  }
-
-  var ident = value.toString().slice(0) // create copy
-
+export function ident(ident: string) {
   // do not quote a valid, unquoted identifier
-  if (/^[a-z_][a-z0-9_$]*$/.test(ident) === true && isReserved(ident) === false) {
+  if (/^[a-z_][a-z0-9_$]*$/.test(ident) === true && !isReserved(ident)) {
     return ident
   }
 
-  var quoted = '"'
-
-  for (var i = 0; i < ident.length; i++) {
-    var c = ident[i]
-    if (c === '"') {
-      quoted += c + c
-    } else {
-      quoted += c
-    }
-  }
-
-  quoted += '"'
-
-  return quoted
+  return `"${ident.replace(/"/g, '""')}"`
 }
 
 function isReserved(value) {
