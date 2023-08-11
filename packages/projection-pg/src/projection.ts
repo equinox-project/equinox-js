6c5e0e2ece4b95e2aac6659f8485e0d2d2341986
Einar Norðfjörð <einar@nordfjord.io>
2023-08-11 12:02:11 -0400

    drop knex dependency (#20)

--- packages/projection-pg/src/projection.ts
+++ packages/projection-pg/src/projection.ts
@@ -1,10 +1,7 @@
-import createKnex, { Knex } from "knex"
 import { Pool } from "pg"
 import { collapseChanges } from "./collapse"
 import { Action, Change } from "./types"
-
-// instantiate knex without a db connection since we're only using it for query building
-const knex = createKnex({ client: "pg" })
+import { ident } from "./quote"
 
 interface Projection {
   table: string
@@ -13,52 +10,114 @@ interface Projection {
   version?: string
 }
 
-function addVersion(
-  projection: Projection,
-  data: Record<string, any>,
-  qb: Knex.QueryBuilder<any, any>,
-) {
-  if (!projection.version) return qb
-  const fullTableName = projection.schema
-    ? `${projection.schema}.${projection.table}`
-    : projection.table
-  const fullColumnName = `${fullTableName}.${projection.version}`
-  return qb.andWhere(fullColumnName, "<", data[projection.version])
+function tableName(projection: Projection) {
+  const schema = ident(projection.schema || "public")
+  return `${schema}.${ident(projection.table)}`
+}
+
+function update(projection: Projection, change: Change) {
+  const table = tableName(projection)
+  const pkeyColumns: string[] = []
+  const pkeyValues: any[] = []
+  const dataColumns: string[] = []
+  const dataValues: any[] = []
+  let versionIdx = -1
+
+  for (const [key, value] of Object.entries(change.data)) {
+    if (projection.id.includes(key)) {
+      pkeyColumns.push(ident(key))
+      pkeyValues.push(value)
+    } else {
+      if (key === projection.version) {
+        versionIdx = dataColumns.length
+      }
+      dataColumns.push(ident(key))
+      dataValues.push(value)
+    }
+  }
+
+  const setClause = dataColumns.map((col, i) => `${col} = $${i + 1}`).join(", ")
+  const whereClause = pkeyColumns
+    .map((col, i) => `${col} = $${i + 1 + dataColumns.length}`)
+    .join(" and ")
+  const values = [...dataValues, ...pkeyValues]
+  let query = `update ${table} set ${setClause} where ${whereClause}`
+  if (versionIdx >= 0) {
+    const col = dataColumns[versionIdx]
+    query += ` and ${col} < $${versionIdx + 1}`
+  }
+  return { text: query, values }
+}
+
+function insert(projection: Projection, change: Change) {
+  const table = tableName(projection)
+  const columns: string[] = []
+  const values: any[] = []
+  for (const [key, value] of Object.entries(change.data)) {
+    columns.push(ident(key))
+    values.push(value)
+  }
+  const pkeyColumns = projection.id.map(ident).join(", ")
+  const quotedColumns = columns.join(", ")
+  const valuesClause = values.map((_, i) => `$${i + 1}`).join(", ")
+  const query = `insert into ${table} (${quotedColumns}) values (${valuesClause}) on conflict (${pkeyColumns}) do nothing`
+  return { text: query, values }
+}
+
+function del(projection: Projection, change: Change) {
+  const table = tableName(projection)
+  const columns: string[] = []
+  const values: any[] = []
+  for (const [key, value] of Object.entries(change.data)) {
+    columns.push(ident(key))
+    values.push(value)
+  }
+
+  const whereClause = columns.map((col, i) => `${col} = $${i + 1}`).join(" and ")
+  const query = `delete from ${table} where ${whereClause}`
+  return { text: query, values }
+}
+
+function upsert(projection: Projection, change: Change) {
+  const table = tableName(projection)
+  const columns: string[] = []
+  const values: any[] = []
+  for (const [key, value] of Object.entries(change.data)) {
+    columns.push(ident(key))
+    values.push(value)
+  }
+  const pkeyColumns = projection.id.map(ident).join(", ")
+  const quotedColumns = columns.join(", ")
+  const valuesClause = values.map((_, i) => `$${i + 1}`).join(", ")
+  let query = `insert into ${table} (${quotedColumns}) values (${valuesClause})`
+  query += ` on conflict (${pkeyColumns}) do update set `
+  query += columns.map((col) => `${col} = EXCLUDED.${col}`).join(", ")
+  if (projection.version) {
+    const versionColumn = ident(projection.version)
+    query += ` where ${table}.${versionColumn} < EXCLUDED.${versionColumn}`
+  }
+  return { text: query, values }
 }
 
 function changeToQuery(projection: Projection, change: Change) {
-  const qb = knex.table(projection.table)
-  if (projection.schema) qb.withSchema(projection.schema)
   switch (change.type) {
     case Action.Update:
-      return addVersion(
-        projection,
-        change.data,
-        qb
-          .where(Object.fromEntries(projection.id.map((col) => [col, change.data[col]])))
-          .update(change.data),
-      )
+      return update(projection, change)
     case Action.Insert:
-      if (!projection.version) {
-        return qb.insert(change.data).onConflict(projection.id).ignore()
-      }
+      if (!projection.version) return insert(projection, change)
     // intentional fallthrough in case of versioned entity
     case Action.Upsert:
-      return addVersion(
-        projection,
-        change.data,
-        qb.insert(change.data).onConflict(projection.id).merge(),
-      )
+      return upsert(projection, change)
     case Action.Delete:
-      return qb.where(change.data).delete()
+      return del(projection, change)
   }
 }
 
 export async function executeChanges(projection: Projection, pool: Pool, changes: Change[]) {
   const change = collapseChanges(changes)
   if (change == null) return
-  const query = changeToQuery(projection, change).toSQL().toNative()
-  await pool.query(query.sql, query.bindings as any[])
+  const query = changeToQuery(projection, change)
+  await pool.query(query)
 }
 
 export function createProjection<T1, T2>(
