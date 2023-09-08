1ddd1c60b7b1b528a14dfcebb26e96068dfc45e5
Einar Norðfjörð <gloogankle@gmail.com>
2023-09-07 23:22:18 -0400

    fix deps

--- packages/dynamo-store/infra/bin/src.ts
+++ packages/dynamo-store/infra/bin/src.ts
@@ -4,7 +4,7 @@ import * as cdk from "aws-cdk-lib"
 import { DynamoStoreStack } from "../lib/stack"
 
 const app = new cdk.App()
-new DynamoStoreStack(app, "DunamoStoreStack", {
+new DynamoStoreStack(app, "DynamoStoreStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
