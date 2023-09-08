9accca5dbe000d2f636a18c6721dd3a39d7619e6
Einar Norðfjörð <gloogankle@gmail.com>
2023-09-07 23:52:52 -0400

    bump version

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -9,7 +9,7 @@ update_version() {
   jq --arg version "$version" '.version = $version' "$dir/package.json" > "$dir/package.json.tmp" && mv "$dir/package.json.tmp" "$dir/package.json"
 }
 # For each directory under ./packages/
-for pkg in ./packages/**/package.json; do
+for pkg in ./packages/*/*/package.json; do
   update_version $pkg
 done
 for pkg in ./packages/*/package.json; do
