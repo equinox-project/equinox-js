67fa8b36f34231551c57be59ef9bca9a2ad9181a
Einar Norðfjörð <gloogankle@gmail.com>
2023-09-07 23:40:48 -0400

    bump version

--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -2,7 +2,8 @@
 
 version=$1
 # For each directory under ./packages/
-for dir in ./packages/*; do
+for pkg in ./packages/**/*/package.json; do
+  dir=$(dirname $pkg)
   # Get the version from the package.json file
   # Set the version in the package.json file
   jq --arg version "$version" '.version = $version' "$dir/package.json" > "$dir/package.json.tmp" && mv "$dir/package.json.tmp" "$dir/package.json"
