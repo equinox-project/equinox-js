5920bd4833502e6f56517d3522b8a3de7615af8d
Einar Norðfjörð <gloogankle@gmail.com>
2023-12-18 15:55:24 -0500

    cleanup: use a class for the stream

--- packages/core/src/lib/Category.ts
+++ packages/core/src/lib/Category.ts
@@ -1,12 +1,16 @@
 import { IStream, TokenAndState, StreamToken, SyncResult } from "./Core.js"
 import { trace } from "@opentelemetry/api"
-import * as Tags from './Tags.js'
+import * as Tags from "./Tags.js"
 import { StreamId } from "./StreamId.js"
 
 /** Store-agnostic interface representing interactions an Application can have with a set of streams with a given pair of Event and State types */
 export interface ICategory<Event, State, Context = void> {
   /** Obtain the state from the target stream */
-  load(streamId: StreamId, maxStaleMs: number, requireLeader: boolean): Promise<TokenAndState<State>>
+  load(
+    streamId: StreamId,
+    maxStaleMs: number,
+    requireLeader: boolean,
+  ): Promise<TokenAndState<State>>
 
   /**
    * Given the supplied `token` [and related `originState`], attempt to move to state `state'` by appending the supplied `events` to the underlying stream
@@ -22,6 +26,38 @@ export interface ICategory<Event, State, Context = void> {
   ): Promise<SyncResult<State>>
 }
 
+class Stream<Event, State, Context> implements IStream<Event, State> {
+  constructor(
+    private inner: ICategory<Event, State, Context>,
+    private empty: TokenAndState<State>,
+    private streamId: StreamId,
+    private context: Context,
+  ) {}
+
+  loadEmpty(): TokenAndState<State> {
+    return this.empty
+  }
+
+  load(maxStaleMs: number, requireLeader: boolean): Promise<TokenAndState<State>> {
+    trace.getActiveSpan()?.setAttributes({
+      [Tags.stream_id]: this.streamId,
+      [Tags.requires_leader]: requireLeader,
+      [Tags.cache_hit]: false,
+      [Tags.allow_stale]: maxStaleMs == Number.MAX_SAFE_INTEGER,
+    })
+    return this.inner.load(this.streamId, maxStaleMs, requireLeader)
+  }
+
+  sync(attempt: number, origin: TokenAndState<State>, events: Event[]): Promise<SyncResult<State>> {
+    trace.getActiveSpan()?.setAttributes({
+      [Tags.stream_id]: this.streamId,
+      [Tags.sync_retries]: attempt > 1 ? attempt - 1 : undefined,
+      [Tags.append_count]: events.length,
+    })
+    return this.inner.sync(this.streamId, this.context, origin.token, origin.state, events)
+  }
+}
+
 export class Category<Event, State, Context = void> {
   constructor(
     private readonly inner: ICategory<Event, State, Context>,
@@ -29,25 +65,6 @@ export class Category<Event, State, Context = void> {
   ) {}
 
   stream(context: Context, streamId: StreamId): IStream<Event, State> {
-    return {
-      loadEmpty: () => this.empty,
-      load: (maxStaleMs, requireLeader) => {
-        trace.getActiveSpan()?.setAttributes({
-          [Tags.stream_id]: streamId,
-          [Tags.requires_leader]: requireLeader,
-          [Tags.cache_hit]: false,
-          [Tags.allow_stale]: maxStaleMs == Number.MAX_SAFE_INTEGER,
-        })
-        return this.inner.load(streamId, maxStaleMs, requireLeader)
-      },
-      sync: (attempt, origin, events) => {
-        trace.getActiveSpan()?.setAttributes({
-          [Tags.stream_id]: streamId,
-          [Tags.sync_retries]: attempt > 1 ? attempt - 1 : undefined,
-          [Tags.append_count]: events.length,
-        })
-        return this.inner.sync(streamId, context, origin.token, origin.state, events)
-      },
-    }
+    return new Stream(this.inner, this.empty, streamId, context)
   }
 }
