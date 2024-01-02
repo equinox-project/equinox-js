46e40a189b9d554e903e933a486976797319c992
Einar Norðfjörð <gloogankle@gmail.com>
2024-01-02 16:24:36 -0500

    feat: actually validate uuids

--- packages/core/src/lib/Uuid.ts
+++ packages/core/src/lib/Uuid.ts
@@ -2,6 +2,33 @@ import { randomUUID } from "crypto"
 
 export type Uuid<T> = string & { __brand: T }
 
+const zero = "0".charCodeAt(0)
+const nine = "9".charCodeAt(0)
+
+const isDigit = (c: number) => zero <= c && c <= nine
+const isHex = (c: number) => isDigit(c) || (97 <= c && c <= 102)
+
+function parse<T>(str: string): Uuid<T> {
+  str = str.toLowerCase()
+  if (str.length !== 36 && str.length !== 32) {
+    throw new Error("Invalid UUID")
+  }
+  let dashCount = 0
+  for (let i = 0; i < str.length; ++i) {
+    if (str[i] === "-") {
+      ++dashCount
+      continue
+    }
+    if (!isHex(str.charCodeAt(i))) {
+      throw new Error("Invalid UUID")
+    }
+  }
+  if (dashCount !== 4 && dashCount !== 0) {
+    throw new Error("Invalid UUID")
+  }
+  return str as Uuid<T>
+}
+
 export type UuidModule<T> = {
   create: () => Uuid<T>
   toString: (uuid: Uuid<T>) => string
@@ -10,12 +37,13 @@ export type UuidModule<T> = {
 
 export type Id<T> = T extends UuidModule<infer F> ? Uuid<F> : never
 
-const uuid = {
-  create: <T>() => randomUUID() as Uuid<T>,
-  toString: <T>(uuid: Uuid<T>) => uuid as string,
-  parse: <T>(uuid: string) => uuid.toLowerCase() as Uuid<T>,
-}
-
-export function create<T>() {
-  return uuid as UuidModule<T>
+export function create<T extends string>(brand: T) {
+  const create = () => randomUUID() as Uuid<T>
+  const toString = (uuid: Uuid<T>) => uuid as string
+  return {
+    create,
+    toString,
+    parse: parse<T>,
+    brand: brand,
+  }
 }
