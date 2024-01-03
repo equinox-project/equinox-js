ad3f77d317c5af15c287ff1877b5841d36b2cf2f
Einar Norðfjörð <gloogankle@gmail.com>
2024-01-02 21:19:44 -0500

    error messages

--- packages/core/src/lib/Uuid.ts
+++ packages/core/src/lib/Uuid.ts
@@ -2,16 +2,20 @@ import { randomUUID } from "crypto"
 
 export type Uuid<T> = string & { __brand: T }
 
-const zero = "0".charCodeAt(0)
-const nine = "9".charCodeAt(0)
-
-const isDigit = (c: number) => zero <= c && c <= nine
-const isHex = (c: number) => isDigit(c) || (97 <= c && c <= 102)
+namespace Validate {
+  const zero = "0".charCodeAt(0)
+  const nine = "9".charCodeAt(0)
+  const a = "a".charCodeAt(0)
+  const f = "f".charCodeAt(0)
+  const isDigit = (c: number) => zero <= c && c <= nine
+  const isHexLetter = (c: number) => a <= c && c <= f
+  export const isHex = (c: number) => isDigit(c) || isHexLetter(c)
+}
 
 function parse<T>(str: string): Uuid<T> {
   str = str.toLowerCase()
   if (str.length !== 36 && str.length !== 32) {
-    throw new Error("Invalid UUID")
+    throw new Error(`Uuid: expected 32 or 36 characters but got ${str.length}`)
   }
   let dashCount = 0
   for (let i = 0; i < str.length; ++i) {
@@ -19,12 +23,12 @@ function parse<T>(str: string): Uuid<T> {
       ++dashCount
       continue
     }
-    if (!isHex(str.charCodeAt(i))) {
-      throw new Error("Invalid UUID")
+    if (!Validate.isHex(str.charCodeAt(i))) {
+      throw new Error(`Uuid: expected hex character but got '${str[i]}' at position ${i}`)
     }
   }
   if (dashCount !== 4 && dashCount !== 0) {
-    throw new Error("Invalid UUID")
+    throw new Error(`Uuid: expected 0 or 4 dashes but got ${dashCount}`)
   }
   return str as Uuid<T>
 }
