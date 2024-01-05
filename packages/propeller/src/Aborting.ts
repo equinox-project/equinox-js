5fe701637b427a22443c6654302c0e576d8e33be
Einar Norðfjörð <gloogankle@gmail.com>
2024-01-04 22:07:57 -0500

    unnecessary file

--- packages/propeller/src/Aborting.ts
+++
@@ -1,7 +0,0 @@
-export const linkSignal = (ctrl: AbortController, signal: AbortSignal) => {
-  const abort = () => ctrl.abort()
-  signal.addEventListener("abort", abort)
-  ctrl.signal.addEventListener("abort", () => {
-    signal.removeEventListener("abort", abort)
-  })
-}
