ebf0e8e6e9e475c0e223ab633b99039ed56e19ee
Einar Nordfjord <gloogankle@gmail.com>
2024-02-25 13:15:26 -0500

    test cleanup

--- packages/message-db/message-db/test/snapshot.test.ts
+++ packages/message-db/message-db/test/snapshot.test.ts
@@ -4,50 +4,35 @@ import { test, expect } from "vitest"
 import * as Snapshot from "../src/lib/Snapshot.js"
 import * as Token from "../src/lib/Token.js"
 
-test("Properly loads snapshot version from previous schema", async () => {
-  const event: ITimelineEvent = {
-    id: randomUUID(),
-    index: 0n,
-    isUnfold: true,
-    size: 0,
-    time: new Date(),
-    type: "Snapshotted",
-    data: JSON.stringify({ hello: "world" }),
-    meta: JSON.stringify({ streamVersion: "0" }),
-  }
+const createEvent = (meta: Snapshot.Meta): ITimelineEvent => ({
+  id: randomUUID(),
+  index: 0n,
+  isUnfold: true,
+  size: 0,
+  time: new Date(),
+  type: "Snapshotted",
+  data: JSON.stringify({ hello: "world" }),
+  meta: JSON.stringify(meta),
+})
 
+test("Properly loads snapshot version from previous schema", async () => {
+  const event = createEvent({ streamVersion: "10" })
   const codec = Codec.json<any>()
 
   const decoded = Snapshot.decode(codec.decode, [event])
   if (decoded == null) throw new Error("Expected snapshot to be decoded")
   const [token, value] = decoded
-  expect(Token.version(token)).toBe(1n)
-  expect(value).toEqual({
-    type: "Snapshotted",
-    data: { hello: "world" },
-  })
+  expect(Token.version(token)).toBe(11n)
+  expect(value).toEqual({ type: "Snapshotted", data: { hello: "world" } })
 })
 
 test("Properly loads snapshot version from new schema", async () => {
-  const event: ITimelineEvent = {
-    id: randomUUID(),
-    index: 0n,
-    isUnfold: true,
-    size: 0,
-    time: new Date(),
-    type: "Snapshotted",
-    data: JSON.stringify({ hello: "world" }),
-    meta: JSON.stringify({ version: "10" }),
-  }
-
+  const event = createEvent({ version: "10" })
   const codec = Codec.json<any>()
 
   const decoded = Snapshot.decode(codec.decode, [event])
   if (decoded == null) throw new Error("Expected snapshot to be decoded")
   const [token, value] = decoded
   expect(Token.version(token)).toBe(10n)
-  expect(value).toEqual({
-    type: "Snapshotted",
-    data: { hello: "world" },
-  })
+  expect(value).toEqual({ type: "Snapshotted", data: { hello: "world" } })
 })
