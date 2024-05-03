f057e230814f31a0f077cecbaf0c738a6f19795d
Einar Norðfjörð <einar@nordfjord.io>
2024-05-02 22:06:08 -0400

    chore: simplify setup by using direnv in CI and add a bootstrap script (#83)

--- apps/hotel/migrations/0001-users.sql
+++ apps/hotel/migrations/0001-users.sql
@@ -1,9 +1,9 @@
 DO $$
 BEGIN
-  CREATE ROLE equinox_example WITH LOGIN;
+  CREATE ROLE equinox_hotel WITH LOGIN;
 EXCEPTION
   WHEN duplicate_object THEN
-      RAISE NOTICE 'The equinox_example role already exists';
+      RAISE NOTICE 'The equinox_hotel role already exists';
 END$$;
-alter role equinox_example set search_path to message_store, public;
-grant message_store to equinox_example;
+alter role equinox_hotel set search_path to message_store, public;
+grant message_store to equinox_hotel;
